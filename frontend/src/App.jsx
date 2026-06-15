66de0aa1dd8c61bbbe44a8a70703585050752cfb
josephineeho <josephineeho8@gmail.com>
2026-06-15 17:57:03 +0100

    test

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import {Routes, BrowserRouter, Route} from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+import './App.css'
+import PostJob from "./components/PostJob"
+
+function App() {
+
+  return (
+
+    <div><PostJob/></div>
+      
+  );
+}
+
+export default App
